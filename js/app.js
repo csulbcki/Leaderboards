fdcf58e704c5e01b1a2cf0ecbe4528cac31c1090
kelleymvo <kelleymahal@gmail.com->
2020-07-18 21:53:57 -0700

    Kelley's first commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-//Testing 
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music','movies','sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA',
+    }
+}
 
-
-
-
-
-
-
+console.log(person.address.city);
