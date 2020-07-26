8e1f5c517e1441561ad565e5f1fe767e062f80f9
Renz <krlane.cki@gmail.com>
2020-07-25 21:37:58 -0700

    Updated with practice template for Kelley & Mike!

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    hobbies: ['music','movies','sports'],
-    address: {
-        street: '50 main st',
-        city: 'Boston',
-        state: 'MA',
-    }
-}
+// ** NOTES ** 
+// + "Document" refers to whichever page calls the "Update" Button is on (in thi case, the // //     main.html page 
+// + "btn-update" is an identifier for the button on the main.html page
+// + "addEventListener" is a function that waits for the user to click the button
+// +  mock membership data is completely random & not based on our actual hours lmao
+//  
+// Let me know if you have any questions! - Renz
 
-console.log(person.address.city);
+document.getElementById("btn-update").addEventListener("click", function(){
+    //Write your code here
+    let memberData = [
+        { name: "Kelley", service: 16, fellowship: 20, leadership: 109 },
+        { name: "Mike", service: 65, fellowship: 91, leadership: 92 },
+        { name: "Caitlin", service: 55, fellowship: 27, leadership: 78  },
+        { name: "Koby", service: 36, fellowship: 76, leadership: 56  },
+        { name: "Kari", service: 120, fellowship: 92, leadership: 72 },
+        { name: "Renz", service: 6, fellowship: 9, leadership: 420 },
+      ];
+    
+});
+
